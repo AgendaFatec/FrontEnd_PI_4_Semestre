3db6adeb8126f9262ab431789f0db1e7178c0391
the-gabriel-marques <gabrielmarquesdasilvacontato@gmail.com>
2026-04-07 19:20:28 -0300

    adição página de minhas reservas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom"
-
-import Login from "./pages/login/login";
-import ListaSalasDocentes from "./pages/docentes/listarSalas";
-import ListaSalasTecnico from "./pages/tecnico/listarSalas";
-import LayoutBase from "./components/LayoutBase";
-
-function AppRoutes() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="/login" element={<Login />} />
-
-                <Route element={<LayoutBase />}>
-                    <Route path="/listar-salas-docentes" element={<ListaSalasDocentes />} />
-                    <Route path="/listar-salas-tecnico" element={<ListaSalasTecnico />} />
-                </Route>
-            </Routes>
-        </Router>
-    )
-}
-
-export default AppRoutes;
-=======
 import {
     BrowserRouter as Router,
     Routes,
@@ -59,4 +28,3 @@ function AppRoutes() {
 }
 
 export default AppRoutes
->>>>>>> 1e409f06be5f65fd2f90d53147bcc980f17cde08
